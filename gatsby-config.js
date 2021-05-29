cb3099de586e2b28fae1a133c53efdbff5f8bb24
MarcosN <marcosnery.comp@gmail.com>
2021-05-29 05:51:34 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Impulsiona Seguidores",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-react-helmet",
+  ],
+};
