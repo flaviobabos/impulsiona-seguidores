ba9a1284e755f4264a5718231b47c761834c18b7
Lucas Lima <limalucas454@gmail.com>
2021-06-03 13:14:17 +0000

    initial adding tailwind css

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  siteMetadata: {
-    title: "Impulsiona Seguidores",
-  },
-  plugins: [
-    "gatsby-plugin-netlify-cms",
-    "gatsby-plugin-react-helmet",
-    `gatsby-plugin-emotion`,
-  ],
+    siteMetadata: {
+        title: "Impulsiona Seguidores",
+    },
+    plugins: [
+        "gatsby-plugin-netlify-cms",
+        "gatsby-plugin-react-helmet",
+        `gatsby-plugin-emotion`,
+        'gatsby-plugin-postcss',
+    ],
 };
