56672a3618c2d5ffac0d1b720f260c032f78ff8e
Lucas Lima <limalucas454@gmail.com>
2021-06-03 11:11:03 -0300

    adding initial state for mobile navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
-  
-import React from 'react';
-import style from './styles';
-import {Helmet} from "react-helmet";
+import React from "react";
+import style from "./styles";
+import { Helmet } from "react-helmet";
+import logo from "./imgs/logo.png";
 
 const Header = ({ className }) => (
   <>
@@ -9,11 +9,35 @@ const Header = ({ className }) => (
       <meta charSet="utf-8" />
       <title>Impulsiona Seguidores</title>
     </Helmet>
-    <header className={className}>
-        I am a header
-    </header>
+    <nav className="h-20 bg-black">
+      <div className="container h-full flex items-center">
+        <div className="ml-3">
+          <button>
+            <img className="h-14 w-14" src={logo}></img>
+          </button>
+        </div>
+        <div className="flex w-full justify-end">
+          <button className="mr-2">
+            {" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="#00CD00"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </nav>
   </>
 );
-
 
 export default style(Header);
