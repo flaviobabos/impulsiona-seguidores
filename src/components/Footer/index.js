789d646c2f55043d542ed04c3df98c0194579df1
Lucas Lima <limalucas454@gmail.com>
2021-06-03 11:29:44 -0300

    adding initial footer version for mobile and desktop

---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import style from "./styles";
+import img from "./imgs/seg.png";
+
+const Footer = ({ className }) => (
+  <>
+    <div className="h-64 bg-black flex content-center">
+      <div className="grid-rows-2 mt-8 mx-auto">
+        <img className="h-20" src={img}></img>
+        <p className="text-center text-white text-lg mt-5">
+          Impulsiona Seguidores - 2021
+        </p>
+        <p className="text-center text-white text-lg mt-2">
+          Desenvolvido por{" "}
+          <a className="text-link" href="https://articadev.com">
+            Artica Dev
+          </a>
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default style(Footer);
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+const style =  (Component) => styled(Component)`
+  color: green;
+  font-size: 250px;
+`;
+
+export default style;
