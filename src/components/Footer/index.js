699654404e54cbbcb76e3779c93524b370b40d31
Lucas Lima <limalucas454@gmail.com>
2021-06-05 16:07:48 -0300

    adding alt tag

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ const Footer = ({ className }) => (
   <>
     <div className="h-64 bg-black flex content-center">
       <div className="grid-rows-2 mt-8 mx-auto">
-        <img className="h-20" src={img}></img>
+        <img className="h-20" alt="seguranca do site" src={img}></img>
         <p className="text-center text-white text-lg mt-5">
           Impulsiona Seguidores - 2021
         </p>
