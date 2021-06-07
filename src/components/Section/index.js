2615799ceff4e65fb06b28ad79900dfd15012c3b
Lucas Lima <limalucas454@gmail.com>
2021-06-07 19:31:28 -0300

    adding section component and importing it

---
+++ src/components/Section/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = (props) => {
+    return (
+        <div className={props.altura+" "+props.cor}>
+            
+        </div>
+    )
+}
+
+export default index
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Section from '../components/Section'
 
 export default function Index() {
 
   return (
     <>
       <Header/>
+      <Section altura="h-screen" cor="bg-gradient"/>
       <Footer/>
     </>
   )
