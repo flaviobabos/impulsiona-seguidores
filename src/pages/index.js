9073a61e647dfca6d50dab113e94a2403127c8c0
Lucas Lima <limalucas454@gmail.com>
2021-06-07 21:26:16 -0300

    Calling warranty section on index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import Section from '../components/Section'
+import SectionWarranty from '../components/SectionWarranty'
 
 export default function Index() {
 
   return (
     <>
       <Header/>
-      <Section altura="h-screen" cor="bg-gradient" padding="px-5"/>
+      <SectionWarranty/>
       <Footer/>
     </>
   )
