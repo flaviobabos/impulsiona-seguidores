cf3de1994158c956930197de67556e8ca7489a54
MarcosN <marcosnery.comp@gmail.com>
2021-06-08 21:08:22 -0300

    refact: using emotionjs global layout pattern for global design rules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Layout from "../components/Layout"
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import SectionWarranty from '../components/SectionWarranty'
@@ -6,11 +7,11 @@ import SectionWarranty from '../components/SectionWarranty'
 export default function Index() {
 
   return (
-    <>
+    <Layout>
       <Header/>
       <SectionWarranty/>
       <Footer/>
-    </>
+    </Layout>
   )
 }
 
