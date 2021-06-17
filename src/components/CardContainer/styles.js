18602db6c61efa11e307b38e64b19afc0daba258
Lucas Lima <limalucas454@gmail.com>
2021-06-17 06:08:20 +0000

    adding title and styles

--- src/components/CardContainer/styles.js
+++ src/components/CardContainer/styles.js
@@ -1,8 +1,14 @@
 import styled from '@emotion/styled';
 
-const style =  (Component) => styled(Component)`
-  color: green;
-  font-size: 250px;
+const style = (Component) => styled(Component)`
+h1{
+width: 60vw;
+font-size: 48px;
+font-weight: 400;
+line-height: 58px;
+text-align: center;
+
+}
 `;
 
 export default style;
