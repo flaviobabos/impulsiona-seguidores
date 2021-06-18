7ad3e8c104e0718a22b17ec4de2ec30a5d56ba6e
Lucas Lima <limalucas454@gmail.com>
2021-06-18 14:27:17 +0000

    refactoring navbar and making it sticky

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,81 +1,71 @@
 import React from "react";
 import style from "./styles";
 import { Helmet } from "react-helmet";
-import logo from "./imgs/logo.png";
-import logo_desk from "./imgs/logo_desk.png";
+import logo from "../../images/logo.png";
+import logo_desk from "../../images/logo_desk.png";
+import { ThreeLineHorizontal } from 'akar-icons';
+
 
 const Header = ({ className }) => (
-  <>
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>Impulsiona Seguidores</title>
-      <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
-        rel="stylesheet"
-      ></link>
-    </Helmet>
-    <nav className="h-20 bg-black">
-      <div className="container-fluid h-full flex items-center ">
-        <div className="ml-5">
-          <button className="lg:hidden">
-            <img alt="logo" className="h-14" src={logo}></img>
-          </button>
-          <button className="hidden lg:block">
-            <img alt="logo" className="h-14" src={logo_desk}></img>
-          </button>
-        </div>
-        <div className="flex w-full justify-end lg:justify-center">
-          <button className="mr-5 lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-10 w-10"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="#00CD00"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-          <div className={className}>
-            <div className="flex-row hidden lg:flex">
-              <a href="/">
-                <h4 className="flex items-center text-white hover:text-green active:text-green mr-8">
-                  Inicio
+    <>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Impulsiona Seguidores</title>
+            <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+                rel="stylesheet"
+            ></link>
+        </Helmet>
+        <nav className="h-20 bg-black sticky top-0">
+            <div className="container-fluid h-full flex items-center ">
+                <div className="ml-5">
+                    <button className="lg:hidden">
+                        <img alt="logo" className="h-14" src={logo}></img>
+                    </button>
+                    <button className="hidden lg:block">
+                        <img alt="logo" className="h-14" src={logo_desk}></img>
+                    </button>
+                </div>
+                <div className="flex w-full justify-end lg:justify-center">
+                    <button className="mr-5 lg:hidden">
+                        <ThreeLineHorizontal color={"#00CD00"} size={40} />
+
+                    </button>
+                    <div className={className}>
+                        <div className="flex-row hidden lg:flex">
+                            <a href="/">
+                                <h4 className="flex items-center text-white hover:text-green active:text-green mr-8">
+                                    Inicio
                 </h4>
-              </a>
-              <a href="sobre">
-                <h4 className="flex items-center text-white hover:text-green mr-8">
-                  Sobre Nós
+                            </a>
+                            <a href="sobre">
+                                <h4 className="flex items-center text-white hover:text-green mr-8">
+                                    Sobre Nós
                 </h4>
-              </a>
-              <a href="beneficios">
-                <h4 className="flex items-center text-white hover:text-green mr-8">
-                  Benefícios
+                            </a>
+                            <a href="beneficios">
+                                <h4 className="flex items-center text-white hover:text-green mr-8">
+                                    Benefícios
                 </h4>
-              </a>
-              <a href="garantia">
-                <h4 className="flex items-center text-white hover:text-green mr-8">
-                  Garantia
+                            </a>
+                            <a href="garantia">
+                                <h4 className="flex items-center text-white hover:text-green mr-8">
+                                    Garantia
                 </h4>
-              </a>
-              <a href="contato">
-                <h4 className="flex items-center text-white hover:text-green">
-                  Contato
+                            </a>
+                            <a href="contato">
+                                <h4 className="flex items-center text-white hover:text-green">
+                                    Contato
                 </h4>
-              </a>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col hidden lg:flex w-56"></div>
+                </div>
             </div>
-          </div>
-          <div className="col hidden lg:flex w-56"></div>
-        </div>
-      </div>
-    </nav>
-  </>
+        </nav>
+    </>
 );
 
 export default style(Header);
