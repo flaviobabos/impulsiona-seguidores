dbc2109bbec2e137ce9882565cbefe4289175f00
Lucas Lima <limalucas454@gmail.com>
2021-06-18 17:38:28 +0000

    refactoring few stylings

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,14 +2,12 @@ import styled from '@emotion/styled';
 
 const style = (Component) => styled(Component)`
 
-h4{
-    font-family: Lato;
+.text{
 font-size: 26px;
-font-style: normal;
 font-weight: 700;
 letter-spacing: 0em;
 text-align: left;
-
+cursor:pointer;
 }
 `;
 
