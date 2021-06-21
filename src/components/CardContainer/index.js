82b82dc7183b72537fac32b1fea8e170a6d1921d
Lucas Lima <limalucas454@gmail.com>
2021-06-21 21:26:34 +0000

    minor fix to card container

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -5,7 +5,7 @@ import style from "./styles";
 const CardContainer = ({ className }) => {
     return (
         <div className={className}>
-            <div className="container mx-auto mb-6">
+            <div className="container mx-auto">
                 <h1 className="mx-auto mb-5 lg:mb-10">Pedidos & <strong>Clientes</strong></h1>
                 <div className="flex justify-center items-center flex-col lg:flex-row auto-cols-3">
                     <ClientCard
