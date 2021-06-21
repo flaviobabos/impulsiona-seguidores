90e4c57ff84e4ebfc5b5ba1cf07f46987733ac66
Marcos <marcosnery.comp@gmail.com>
2021-06-21 01:05:40 -0300

    removing netilify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
         title: "Impulsiona Seguidores",
     },
     plugins: [
-        "gatsby-plugin-netlify-cms",
         "gatsby-plugin-react-helmet",
         `gatsby-plugin-emotion`,
         'gatsby-plugin-postcss',
