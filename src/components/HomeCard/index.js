4406f1a3f4fab125edfe9b4f57df4c4324ad5225
Lucas Lima <limalucas454@gmail.com>
2021-06-22 23:34:31 +0000

    adding card animation, and initial slider setting and styling.

--- src/components/HomeCard/index.js
+++ src/components/HomeCard/index.js
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./styles";
 
 const HomeCard = ({ className }) => {
+    let opcao = 0;
+    let titles = ["No que podemos te ajudar?", "De quantos seguidores você precisa?", "De quantas curtidas você precisa?", "De quantas curtidas e seguidores você precisa?"];
+    let followers = ["1000", "1500", "3000", "6000"]
+    let likes = ["25", "50", "100", "200"]
+    let [actualTitle, setTitle] = useState(titles[0]);
+    let [actualFollower, setFollower] = useState(followers[0]);
+    let [actualLike, setLiker] = useState(likes[0]);
+    let [actualCombo, setCombo] = useState(followers[0]);
     return (
         <>
             <div className={className}>
-                <div className="card mb-9 mx-auto">
+                <div className={`card mb-9 mx-auto ${actualTitle === titles[0] ? "" : "card-increase"}`}>
                     <div className="bg-blue block top-0 rounded-t-lg w-full h-20">
-                        <h2 className="text-white text-center pt-6">No que podemos te ajudar?</h2>
+                        <h2 className={`text-white px-4 text-center ${actualTitle === titles[0] ? "pt-6" : "pt-3"}`}>{actualTitle}</h2>
                     </div>
-                    <div className="btn-group flex flex-col items-center mt-12">
-                        <button className="btn text-white mb-5">Seguidores</button>
-                        <button className="btn text-white mb-5">Curtidas</button>
-                        <button className="btn text-white mb-5">Seguidores <br></br>e Curtidas</button>
+                    <div className="flex flex-col items-center mt-12">
+                        {actualTitle === titles[0] &&
+                            <>
+                                <button onClick={() => setTitle(titles[1])} className="btn text-white mb-5">Seguidores</button>
+                                <button onClick={() => setTitle(titles[2])} className="btn text-white mb-5">Curtidas</button>
+                                <button onClick={() => setTitle(titles[3])} className="btn text-white mb-5">Seguidores <br></br>e Curtidas</button>
+                            </>}
+
+                        {actualTitle === titles[1] &&
+                            <h4 className="text-link">{actualFollower + " seguidores por mês"}</h4>
+                        }
+                        <input className="mt-10 w-48" type="range" min="0" max="3" defaultValue="0"></input>
+                        <div className="mt-2">
+                            <div className="flex mt-7 mb-2 items-center ">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#00CD00" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" display="block" id="DoubleCheck"><path d="M2 12l5.25 5 2.625-3" /><path d="M8 12l5.25 5L22 7" /><path d="M16 7l-3.5 4" /></svg>
+                                <p className="text-link ml-2">Seguidores <strong className="text-link">Reais</strong> e <strong className="text-link">Brasileiros</strong></p>
+                            </div>
+                            <div className="flex mb-2 items-center ">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#00CD00" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" display="block" id="DoubleCheck"><path d="M2 12l5.25 5 2.625-3" /><path d="M8 12l5.25 5L22 7" /><path d="M16 7l-3.5 4" /></svg>
+                                <p className="text-link ml-2">Quantidade <strong className="text-link">garantida</strong> na assinatura</p>
+                            </div>
+                            <div className="flex mb-2 items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#00CD00" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" display="block" id="DoubleCheck"><path d="M2 12l5.25 5 2.625-3" /><path d="M8 12l5.25 5L22 7" /><path d="M16 7l-3.5 4" /></svg>
+                                <p className="text-link ml-2">Reposição <strong className="text-link">automática</strong> de seguidores</p>
+                            </div>
+                            <div className="flex mb-2 items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#00CD00" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" display="block" id="DoubleCheck"><path d="M2 12l5.25 5 2.625-3" /><path d="M8 12l5.25 5L22 7" /><path d="M16 7l-3.5 4" /></svg>
+                                <p className="text-link ml-2">Atendimento via <strong className="text-link">e-mail</strong> e <strong className="text-link">WhatsApp</strong></p>
+                            </div>
+                            <div className="flex mb-2 items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#00CD00" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" display="block" id="DoubleCheck"><path d="M2 12l5.25 5 2.625-3" /><path d="M8 12l5.25 5L22 7" /><path d="M16 7l-3.5 4" /></svg>
+                                <p className="text-link ml-2">Cancelamento a <strong className="text-link">qualquer momento</strong></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     );
 };
--- src/components/HomeCard/styles.js
+++ src/components/HomeCard/styles.js
@@ -38,6 +38,115 @@ line-height: 29px;
 letter-spacing: 0em;
 }
 
+h4{
+font-size: 20px;
+font-weight: 400;
+line-height: 29px;
+letter-spacing: 0em;
+
+}
+
+p{
+
+
+font-size: 14px;
+font-weight: 400;
+line-height: 18px;
+letter-spacing: 0em;
+
+}
+
+.card-increase{
+    height: 650px;
+    transition: 0.5s;
+}
+
+input[type=range] {
+  -webkit-appearance: none;
+}
+input[type=range]:focus {
+  outline: none;
+}
+input[type=range]::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 12px;
+  cursor: pointer;
+  animate: 0.2s;
+  box-shadow: 0px 0px 0px #000000;
+  background: #ECECEC;
+  border-radius: 10px;
+  border: 0px solid #000000;
+}
+input[type=range]::-webkit-slider-thumb {
+  box-shadow: 0px 0px 1px #000000;
+  border: 0px solid #000000;
+  height: 22px;
+  width: 22px;
+  border-radius: 50px;
+  background: #F2295B;
+  cursor: pointer;
+  -webkit-appearance: none;
+  margin-top: -5px;
+}
+input[type=range]:focus::-webkit-slider-runnable-track {
+  background: #ECECEC;
+}
+input[type=range]::-moz-range-track {
+  width: 100%;
+  height: 12px;
+  cursor: pointer;
+  animate: 0.2s;
+  box-shadow: 0px 0px 0px #000000;
+  background: #ECECEC;
+  border-radius: 10px;
+  border: 0px solid #000000;
+}
+input[type=range]::-moz-range-thumb {
+  box-shadow: 0px 0px 1px #000000;
+  border: 0px solid #000000;
+  height: 22px;
+  width: 22px;
+  border-radius: 50px;
+  background: #F2295B;
+  cursor: pointer;
+}
+input[type=range]::-ms-track {
+  width: 100%;
+  height: 12px;
+  cursor: pointer;
+  animate: 0.2s;
+  background: transparent;
+  border-color: transparent;
+  color: transparent;
+}
+input[type=range]::-ms-fill-lower {
+  background: #ECECEC;
+  border: 0px solid #000000;
+  border-radius: 20px;
+  box-shadow: 0px 0px 0px #000000;
+}
+input[type=range]::-ms-fill-upper {
+  background: #ECECEC;
+  border: 0px solid #000000;
+  border-radius: 20px;
+  box-shadow: 0px 0px 0px #000000;
+}
+input[type=range]::-ms-thumb {
+  box-shadow: 0px 0px 1px #000000;
+  border: 0px solid #000000;
+  height: 22px;
+  width: 22px;
+  border-radius: 50px;
+  background: #F2295B;
+  cursor: pointer;
+}
+input[type=range]:focus::-ms-fill-lower {
+  background: #ECECEC;
+}
+input[type=range]:focus::-ms-fill-upper {
+  background: #ECECEC;
+}
+
 `;
 
 export default style;
