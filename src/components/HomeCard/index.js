63f5f9b497668a13375851439425fc0cba43ae4b
Lucas Lima <limalucas454@gmail.com>
2021-06-24 05:45:18 +0000

    changing variables to object

--- src/components/HomeCard/index.js
+++ src/components/HomeCard/index.js
@@ -5,36 +5,49 @@ import style from "./styles";
 
 const HomeCard = ({ className }) => {
     const [valor, setState] = useState({ x: 0 });
-    let titles = ["No que podemos te ajudar?", "De quantos seguidores você precisa?", "De quantas curtidas você precisa?", "De quantas curtidas e seguidores você precisa?"];
-    let followers = ["1000", "1500", "3000", "6000"]
-    let likes = ["25", "50", "100", "200"]
-    let followersprices = ["55,90", "75,90", "145,90", "279,90"]
-    let likesprices = ["25,90", "45,90", "84,90", "159,90"]
-    let combosprices = ["89,90", "125,90", "249,90", "489,90"]
-    let linksfollower = ["http://mon.net.br/17s8xk", "http://mon.net.br/17s8xl", "http://mon.net.br/17s8xm", "http://mon.net.br/17s8xn"]
-    let linkslikes = ["http://mon.net.br/17s8xv", "http://mon.net.br/17s8xx", "http://mon.net.br/17s8y0", "http://mon.net.br/17s8y1"]
-    let linkscombos = ["http://mon.net.br/17s8y3", "http://mon.net.br/17s8y4", "http://mon.net.br/17s8y5", "http://mon.net.br/17s8y7"]
-    let [actualTitle, setTitle] = useState(titles[0]);
+    let products = [
+        {
+            titles: ["No que podemos te ajudar?", "De quantos seguidores você precisa?", "De quantas curtidas você precisa?", "De quantas curtidas e seguidores você precisa?"]
+        },
+        {
+            followers: ["1000", "1500", "3000", "6000"],
+            followersprices: ["55,90", "75,90", "145,90", "279,90"],
+            followerslinks: ["http://mon.net.br/17s8xk", "http://mon.net.br/17s8xl", "http://mon.net.br/17s8xm", "http://mon.net.br/17s8xn"]
+        },
+        {
+            likes: ["25", "50", "100", "200"],
+            likesprices: ["25,90", "45,90", "84,90", "159,90"],
+            linkslikes: ["http://mon.net.br/17s8xv", "http://mon.net.br/17s8xx", "http://mon.net.br/17s8y0", "http://mon.net.br/17s8y1"]
+        },
+        {
+            combos: ["1000", "1500", "3000", "6000"],
+            combosprices: ["89,90", "125,90", "249,90", "489,90"],
+            linkscombos: ["http://mon.net.br/17s8y3", "http://mon.net.br/17s8y4", "http://mon.net.br/17s8y5", "http://mon.net.br/17s8y7"]
+        }
+    ]
+
+
+    let [actualTitle, setTitle] = useState(products.titles[0]);
     return (
         <>
             <div className={className}>
-                <div className={`card mb-9 mx-auto ${actualTitle === titles[0] ? "" : "card-increase"}`}>
+                <div className={`card mb-9 mx-auto ${actualTitle === products.titles[0] ? "" : "card-increase"}`}>
                     <div className="bg-blue block top-0 rounded-t-lg w-full h-20">
-                        <h2 className={`text-white px-4 text-center ${actualTitle === titles[0] ? "pt-6" : "pt-3 lg:pt-6"}`}>{actualTitle}</h2>
+                        <h2 className={`text-white px-4 text-center ${actualTitle === products.titles[0] ? "pt-6" : "pt-3 lg:pt-6"}`}>{actualTitle}</h2>
                     </div>
-                    <div className={`flex flex-col items-center mt-12 ${actualTitle !== titles[0] ? "lg:mt-1" : ""}`}>
-                        {actualTitle === titles[0] &&
+                    <div className={`flex flex-col items-center mt-12 ${actualTitle !== products.titles[0] ? "lg:mt-1" : ""}`}>
+                        {actualTitle === products.titles[0] &&
                             <>
-                                <button onClick={() => setTitle(titles[1])} className="btn text-white mb-5">Seguidores</button>
-                                <button onClick={() => setTitle(titles[2])} className="btn text-white mb-5">Curtidas</button>
-                                <button onClick={() => setTitle(titles[3])} className="btn text-white mb-5">Seguidores <br></br>e Curtidas</button>
+                                <button onClick={() => setTitle(products.titles[1])} className="btn text-white mb-5">Seguidores</button>
+                                <button onClick={() => setTitle(products.titles[2])} className="btn text-white mb-5">Curtidas</button>
+                                <button onClick={() => setTitle(products.titles[3])} className="btn text-white mb-5">Seguidores <br></br>e Curtidas</button>
                             </>}
 
-                        {actualTitle === titles[1] &&
+                        {actualTitle === products.titles[1] &&
                             <>
                                 <div className="lg:grid lg:grid-cols-2 lg:items-center">
                                     <div className="flex flex-col justify-center items-center">
-                                        <h4 className="text-link">{followers[valor.x] + " seguidores por mês"}</h4>
+                                        <h4 className="text-link">{products.followers[valor.x] + " seguidores por mês"}</h4>
                                         <Slider
                                             className="mt-10"
                                             axis="x"
@@ -62,7 +75,7 @@ const HomeCard = ({ className }) => {
                                             <h6 className="flex items-center mr-2">R$
 </h6>
                                             <h3 className="flex items-center">
-                                                {followersprices[valor.x]}
+                                                {products.followersprices[valor.x]}
                                             </h3>
 
                                             <h6 className="flex items-end">/Mês
@@ -94,20 +107,20 @@ const HomeCard = ({ className }) => {
                                             <p className="text-link ml-2 texto"><strong className="text-pink">Atenção:</strong> Seu <strong className="text-pink">perfil</strong> precisa estar <strong className="text-pink">público</strong></p>
                                         </div>
                                         <div className="flex justify-center items-center flex-col mt-10">
-                                            <a href={linksfollower[valor.x]}>
+                                            <a href={products.followerslinks[valor.x]}>
                                                 <button className="btn text-white mb-5">Contratar</button>
                                             </a>
-                                            <button onClick={() => setTitle(titles[0])} className="btn-inverse text-pink mb-5">Voltar</button>
+                                            <button onClick={() => setTitle(products.titles[0])} className="btn-inverse text-pink mb-5">Voltar</button>
                                         </div>
                                     </div>
                                 </div>
                             </>
                         }
-                        {actualTitle === titles[2] &&
+                        {actualTitle === products.titles[2] &&
                             <>
                                 <div className="lg:grid lg:grid-cols-2 lg:items-center">
                                     <div className="flex flex-col justify-center items-center">
-                                        <h4 className="text-link">{likes[valor.x] + " curtidas por mês"}</h4>
+                                        <h4 className="text-link">{products.likes[valor.x] + " curtidas por mês"}</h4>
                                         <Slider
                                             className="mt-10"
                                             axis="x"
@@ -135,7 +148,7 @@ const HomeCard = ({ className }) => {
                                             <h6 className="flex items-center mr-2">R$
 </h6>
                                             <h3 className="flex items-center">
-                                                {likesprices[valor.x]}
+                                                {products.likesprices[valor.x]}
                                             </h3>
 
                                             <h6 className="flex items-end">/Mês
@@ -167,20 +180,20 @@ const HomeCard = ({ className }) => {
                                             <p className="text-link ml-2 texto"><strong className="text-pink">Atenção:</strong> Seu <strong className="text-pink">perfil</strong> precisa estar <strong className="text-pink">público</strong></p>
                                         </div>
                                         <div className="flex justify-center items-center flex-col mt-10">
-                                            <a href={linkslikes[valor.x]}>
+                                            <a href={products.linkslikes[valor.x]}>
                                                 <button className="btn text-white mb-5">Contratar</button>
                                             </a>
-                                            <button onClick={() => setTitle(titles[0])} className="btn-inverse text-pink mb-5">Voltar</button>
+                                            <button onClick={() => setTitle(products.titles[0])} className="btn-inverse text-pink mb-5">Voltar</button>
                                         </div>
                                     </div>
                                 </div>
                             </>
                         }
-                        {actualTitle === titles[3] &&
+                        {actualTitle === products.titles[3] &&
                             <>
                                 <div className="lg:grid lg:grid-cols-2 lg:items-center">
                                     <div className="flex flex-col justify-center items-center">
-                                        <h4 className="text-link text-center px-5">{followers[valor.x] + " seguidores e " + likes[valor.x] + " curtidas por mês"}</h4>
+                                        <h4 className="text-link text-center px-5">{products.followers[valor.x] + " seguidores e " + products.likes[valor.x] + " curtidas por mês"}</h4>
                                         <Slider
                                             className="mt-10"
                                             axis="x"
@@ -208,7 +221,7 @@ const HomeCard = ({ className }) => {
                                             <h6 className="flex items-center mr-2">R$
 </h6>
                                             <h3 className="flex items-center">
-                                                {combosprices[valor.x]}
+                                                {products.combosprices[valor.x]}
                                             </h3>
 
                                             <h6 className="flex items-end">/Mês
@@ -240,10 +253,10 @@ const HomeCard = ({ className }) => {
                                             <p className="text-link ml-2 texto"><strong className="text-pink">Atenção:</strong> Seu <strong className="text-pink">perfil</strong> precisa estar <strong className="text-pink">público</strong></p>
                                         </div>
                                         <div className="flex justify-center items-center flex-col mt-10">
-                                            <a href={linkscombos[valor.x]}>
+                                            <a href={products.linkscombos[valor.x]}>
                                                 <button className="btn text-white mb-5">Contratar</button>
                                             </a>
-                                            <button onClick={() => setTitle(titles[0])} className="btn-inverse text-pink mb-5">Voltar</button>
+                                            <button onClick={() => setTitle(products.titles[0])} className="btn-inverse text-pink mb-5">Voltar</button>
                                         </div>
                                     </div>
                                 </div>
