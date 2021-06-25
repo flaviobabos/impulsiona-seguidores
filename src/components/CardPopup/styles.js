b82255874a862aa659f0c28ca55c2253a0462625
Lucas Lima <limalucas454@gmail.com>
2021-06-25 21:29:32 +0000

    adding initial popup structure and more

--- src/components/CardPopup/styles.js
+++ src/components/CardPopup/styles.js
@@ -3,8 +3,8 @@ import styled from "@emotion/styled";
 const style = (Component) => styled(Component)`
 
 .card{
-    height: 400px;
-width: 300px;
+    height: 330px;
+width: 100%;
 background: #FFFFFF;
 border-radius: 11px;
 filter: drop-shadow(4px 11px 10px rgba(0, 0, 0, 0.125));
@@ -47,56 +47,11 @@ letter-spacing: 0em;
 
 }
 
-h3{
-    color: #717171;
-font-size: 40px;
-font-weight: 700;
-line-height: 58px;
-letter-spacing: 0em;
-text-align: left;
-}
-h6{
-    color: #717171;
-font-size: 14px;
-font-weight: 700;
-line-height: 17px;
-letter-spacing: 0em;
-text-align: left;
 
+.modal > .content {
+  width: 100%;
+  padding:0px;
 }
-
-.texto{
-font-size: 14px;
-font-weight: 400;
-line-height: 18px;
-letter-spacing: 0em;
-}
-
-.card-increase{
-    height: 100%;
-    transition: 0.5s;
-}
-
-@media (min-width:768px){
-    .card-increase{
-    height:100%;
-    width:620px;
-    transition:0.5s;}
-}
-
-.btn-inverse{
-min-height:50px;
-padding: 5px;
-width: 200px;
-border-radius: 5px;
-background: #FFF;
-box-shadow: 0px 4px 4px 0px #00000040;
-font-size: 20px;
-font-weight: 400;
-line-height: 29px;
-letter-spacing: 0em;
-}
-
 
 `;
 
