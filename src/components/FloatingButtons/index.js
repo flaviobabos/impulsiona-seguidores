c0bf8e26731b05e606149afc427934a0e7d502bc
Lucas Lima <limalucas454@gmail.com>
2021-06-28 18:40:33 -0300

    minor size increase

--- src/components/FloatingButtons/index.js
+++ src/components/FloatingButtons/index.js
@@ -11,10 +11,10 @@ const FloatingButtons = ({ className }) => {
                 <div className="bottom-0 fixed z-50 right-0">
                     <div className="flex justify-center flex-col">
                         <button className="mb-4 "><a rel="noreferrer" target="_blank" href="https://web.whatsapp.com/send?phone=5544998433415&text=">
-                            <WhatsappFill className="mr-16 rounded-full bg-white" color={"#00E676"} enableBackground={"#FFF"} size={30} /></a></button>
+                            <WhatsappFill className="mr-16 rounded-full bg-white" color={"#00E676"} enableBackground={"#FFF"} size={40} /></a></button>
                         <button>
                         <Link to="home" spy={true} smooth={true} offset={-80} duration={500} >
-                            <ChevronUp className="rounded-full" color={"#00CD00"} size={30} />
+                            <ChevronUp className="rounded-full" color={"#00CD00"} size={40} />
                         </Link></button>
                         </div>
                     </div>
