f9de29f661cf54badb3b2221651552e6ac58c37f
MarcosN <marcosnery.comp@gmail.com>
2021-07-03 06:07:25 -0300

    feat: adding google analytics integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,20 @@
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  });
+  
 module.exports = {
     siteMetadata: {
         title: "Impulsiona Seguidores",
     },
     plugins: [
         "gatsby-plugin-react-helmet",
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: process.env.GOOGLE_ANALYTICS_ID,
+                head: true
+            }
+        },
         `gatsby-plugin-emotion`,
         'gatsby-plugin-postcss',
         {
