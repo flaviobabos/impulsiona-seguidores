de8d79ab1d13059f886b66087ef959aad62da9f6
MarcosN <marcosnery.comp@gmail.com>
2021-07-06 05:21:51 -0300

    fix: fixing fluxodigital form

--- src/components/HomeCard/index.js
+++ src/components/HomeCard/index.js
@@ -12,10 +12,11 @@ const HomeCard = ({ className }) => {
 
        data['api_action'] = 'contact_sync';
        data['api_key'] = process.env.GATSBY_ACTIVE_CAMPAIGN_KEY;
-       axios.post('https://fluxodigital12.api-us1.com/admin/api.php', {data}, 
-            {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }})
-       .then((response) => { console.log(response) });
-      // window.location.replace(products[products[0].titles.indexOf(chosenTitle)].links[valor.x]);
+       const url = 'https://fluxodigital12.api-us1.com/admin/api.php';
+       axios.post(url, {},
+            {params: data},
+            {headers: { 'Content-Type': 'application/x-www-form-urlencoded', "Access-Control-Allow-Origin": "*" }});
+      window.location.replace(products[products[0].titles.indexOf(chosenTitle)].links[valor.x]);
 
       };
     const [valor, setState] = useState({ x: 0 });
